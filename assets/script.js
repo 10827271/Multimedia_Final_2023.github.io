54cd30ac982d2d74f709ddb0721b42750a4856de
10827271 <71808760+10827271@users.noreply.github.com>
2023-06-05 17:47:39 +0800

    開始加圖

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,28 @@
 function copyMenu() {
     var dptCategory = document.querySelector('.dpt-cat');
-    var dptPlace = document.querySelector('.departments')
+    var dptPlace = document.querySelector('.departments');
     dptPlace.innerHTML = dptCategory.innerHTML;
 
     var mainNav = document.querySelector('.header-nav nav');
-    var navPlace = document.querySelector('.off-canvas nav');
+    var navPlace = document.querySelector('.off-canvas nav')
     navPlace.innerHTML = mainNav.innerHTML;
 
-    var topNav = document.querySelector('header-top');
+    var topNav = document.querySelector('.header-top');
     var topPlace = document.querySelector('.off-canvas .thetop-nav')
     topPlace.innerHTML = topNav.innerHTML;
 }
 copyMenu();
+
+const menuButton = document.querySelector('.trigger'),
+    closeButton = document.querySelector('.t-close'),
+    addclass = document.querySelector('.site');
+menuButton.addEventListener('click',function() {
+    addclass.classList.toggle('showmenu')
+})
+closeButton.addEventListener('click' , function() {
+    addclass.classList.remove('showmenu')
+})
+
 
 const submenu = document.querySelectorAll('.has-child .icon-small');
 submenu.forEach((menu) => menu.addEventListener('click',toggle));
@@ -20,5 +31,5 @@ function toggle(e){
     e.preventDefault();
     submenu.forEach((itme) => itme != this ? itme.closest('.has-child').classList.remove('expand') : null);
     if (this.closest('.has-child').classList != 'expand');
-    this.closest('.has-child')
+    this.closest('.has-child').classList.toggle('expand')
 }
